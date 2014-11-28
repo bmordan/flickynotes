5f47722886b6bbb47214c98c83010d95810278b5
Bernard Mordan <bernardmordan@gmail.com>
2014-11-28 09:21:18 +0000

    tighter flow and adjusted the styling

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@
 // });
 
 Meteor.startup(function () {
+  Meteor.call('resetDemoBoard')
   Meteor.call('constructDemoBoard')
 });
 
@@ -23,16 +24,18 @@ Meteor.methods({
   },
   constructDemoBoard: function(){
     if(Boards.getDemo().length === 0){
+
       var id = new Mongo.ObjectID()
       var defaultZones = ["Todo","Doing","Done"]
 
-      Boards.insert({_id: id ,title: "Demo",zones: [],zoneWidth: 0}, function(){
-        _.each(defaultZones, function(newZone){
-          Boards.addZone(id, newZone)
-        })   
-      })
+      Boards.insert({_id: id ,title: "Demo",zones: [],zoneWidth: 0})
 
+      _.each(defaultZones, function(newZone){
+        Boards.addZone(id, newZone)
+      })
     }
-    return true
+  },
+  resetDemoBoard: function(){
+    Boards.remove({title: "Demo"})
   }
 })
